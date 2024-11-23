b328f9e968dd2e8f937a9aefb1b07ce9e04cdb7d
Marcus Ang <marcaroni28@gmail.com>
2024-11-23 14:18:08 +0800

    feat: change projects layout

--- src/pages/projects/data.ts
+++ src/pages/projects/data.ts
@@ -1,88 +1,49 @@
-import type { ProjectData } from '@/types'
-
-export const projectData: ProjectData = [
+export const projectData: Array<{
+  text: string
+  description?: string
+  icon?: string
+  href: string
+}> = [
   {
-    title: 'Projects Group',
-    projects: [
-      {
-        text: 'Project Name',
-        description: 'Your project description information is a long piece of text.',
-        icon: 'i-carbon-campsite',
-        href: '/',
-      },
-      {
-        text: 'Project Name',
-        description: 'Your project description information is a long piece of text.',
-        icon: 'i-carbon-campsite',
-        href: '/',
-      },
-    ],
+    text: 'Trading Simulator',
+    description: '(WIP) A backtesting simulator using historical market data.',
+    icon: 'i-carbon-campsite',
+    href: '/',
   },
   {
-    title: 'Projects Group',
-    projects: [
-      {
-        text: 'Project Name',
-        description: 'Your project description information is a long piece of text.',
-        icon: 'i-carbon-campsite',
-        href: '/',
-      },
-      {
-        text: 'Project Name',
-        description: 'Your project description information is a long piece of text.',
-        icon: 'i-carbon-campsite',
-        href: '/',
-      },
-      {
-        text: 'Project Name',
-        description: 'Your project description information is a long piece of text.',
-        icon: 'i-carbon-campsite',
-        href: '/',
-      },
-    ],
+    text: 'Algorithm & Path Visualiser',
+    description: '(WIP) A visualiser for easier understanding of popular algorithms.',
+    icon: 'i-carbon-campsite',
+    href: '/',
   },
   {
-    title: 'Project Name',
-    projects: [],
+    text: 'Interactive Terminal',
+    description: '(WIP) A real-time monitoring tool that displays indicators of system health.',
+    icon: 'i-carbon-campsite',
+    href: '/',
   },
   {
-    title: 'Projects Group',
-    projects: [
-      {
-        text: 'Project Name',
-        description: 'Your project description information is a long piece of text.',
-        icon: 'i-carbon-campsite',
-        href: '/',
-      },
-      {
-        text: 'Project Name',
-        description: 'Your project description information is a long piece of text.',
-        icon: 'i-carbon-campsite',
-        href: '/',
-      },
-      {
-        text: 'Project Name',
-        description: 'Your project description information is a long piece of text.',
-        icon: 'i-carbon-campsite',
-        href: '/',
-      },
-      {
-        text: 'Project Name',
-        description: 'Your project description information is a long piece of text.',
-        icon: 'i-carbon-campsite',
-        href: '/',
-      },
-    ],
+    text: 'NUS Fintech Summit Website',
+    description: 'Acted as the Technical Lead for the development of this event website.',
+    icon: 'i-carbon-campsite',
+    href: '/',
   },
   {
-    title: 'Projects Group',
-    projects: [
-      {
-        text: 'Project Name',
-        description: 'Your project description information is a long piece of text.',
-        icon: 'i-carbon-campsite',
-        href: '/',
-      },
-    ],
+    text: 'TAHub',
+    description: 'A tool that allows Teaching Assistants (TAs) to manage student information.',
+    icon: 'i-carbon-campsite',
+    href: '/',
+  },
+  {
+    text: 'Beer Logo Detector',
+    description: 'Using Computer Vision to detect logos of famous beers in Ho Chi mInh City, Vietnam.',
+    icon: 'i-carbon-campsite',
+    href: '/',
+  },
+  {
+    text: 'PokeCommerce',
+    description: 'My first full-stack project.',
+    icon: 'i-carbon-campsite',
+    href: '/',
   },
 ]
--- src/types.ts
+++ src/types.ts
@@ -1,19 +1,8 @@
 import type { CollectionEntry } from 'astro:content'
 
+// Retain the types for blogs and pages if they're being used elsewhere
 export type PostKey = 'blog'
-
 export type CollectionPosts = CollectionEntry<PostKey>
 
 export type Pages = 'pages'
-
 export type CollectionPages = CollectionEntry<Pages>
-
-export type ProjectData = Array<{
-  title: string
-  projects: Array<{
-    text: string
-    description?: string
-    icon?: string
-    href: string
-  }>
-}>
